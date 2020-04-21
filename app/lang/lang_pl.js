cc91a632438617303596da72c62b77f4e4159072
Przemek Kozinski <przemekoz@o2.pl>
2020-04-22 00:02:00 +0200

    added : few new translations

--- app/lang/lang_pl.js
+++ app/lang/lang_pl.js
@@ -1,11 +1,22 @@
 export const translations = new Map();
-translations.set('add', 'Dodaj');
-translations.set('elements', 'Elementy');
+translations.set('edit',        'Edytuj');
+translations.set('save',        'Zapisz');
+translations.set('clear',       'Wyczyść');
+translations.set('search',      'Szukaj');
+translations.set('remove',      'Usuń');
+translations.set('back',        'Wstecz');
+translations.set('actions',     'Akcje');
+translations.set('active',      'Aktywny');
+translations.set('inactive',    'Nieaktywny');
+translations.set('yes',         'Tak');
+translations.set('no',          'Nie');
+translations.set('add',         'Dodaj');
+translations.set('elements',    'Elementy');
 translations.set('add-element', 'Dodaj Element');
-translations.set('title', 'Nazwa');
+translations.set('title',       'Nazwa');
 translations.set('description', 'Opis');
-translations.set('tags', 'Tagi');
-translations.set('profile', 'Profil');
-translations.set('logout', 'Wyloguj się');
+translations.set('tags',        'Tagi');
+translations.set('profile',     'Profil');
+translations.set('logout',      'Wyloguj się');
 
 
